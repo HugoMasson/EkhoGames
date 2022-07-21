dc551ed6a7c6ea9e15a57f61c244c235df6c6f2c
HugoMasson <hugomasson2901@gmail.com>
2022-07-21 17:39:59 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Fragment } from 'react';
+import './style/generalStyle.css';
+
+import ConnectionPage from "./pages/ConnectionPage";
+import AddGame from "./pages/AddGame";
+import AddArticle from "./pages/AddArticle";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<ConnectionPage />} />
+          <Route path="addGame" element={<AddGame />} />
+          <Route path="addArticle" element={<AddArticle />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
